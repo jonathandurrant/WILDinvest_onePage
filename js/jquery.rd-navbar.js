fdfe8138bafe4d82b3a567925dbbe27c5cd6bcb4
jonathandurrant <jonathandurrant.sa@gmail.com>
2016-05-04 10:30:02 -0400

    optimizations (ftp uploaded)
    contact us
    alt for images

--- js/jquery.rd-navbar.js
+++ js/jquery.rd-navbar.js
@@ -49,7 +49,7 @@
                     }).append($('<span/>')))
                     .append($('<h2/>', {
                         'class': settings.titleClass                        
-            }).append($('[data-type="rd-navbar-brand"]').length? $('[data-type="rd-navbar-brand"]').html() : "WILDinvest - Getting in the Game"))
+            }).append($('[data-type="rd-navbar-brand"]').length? $('[data-type="rd-navbar-brand"]').html() : "WILDinvest"))
                     );
         },
 
